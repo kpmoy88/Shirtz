521df1745fe46aa2ac64681ab83419363725170f
Salman Ansari <salman.ansari@gmail.com>
2013-07-11 19:58:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shirtz::Application.config.secret_key_base = '544b81ee74b3df47728c27c92451768800ef8bd81dfbc4ea91185798666f04e90845fcd2fc72e8f61eb6e6c75a8f1e61382530cc811cb2dea17fecc897d984ea'
